fa285ad95832db20e6a55b3e6b6fd7a60f9521df
ksy0310007@naver.com <ksy@renocomms.com>
2023-02-08 16:05:06 +0900

    create project add app icon change app name

---
+++ GooglePlayBook_PreProject/GooglePlayBook_PreProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GooglePlayBook_PreProject
+//
+//  Created by 김소영 on 2023/02/08.
+//  Copyright © 2023 SoYoungKim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GooglePlayBook_PreProject/GooglePlayBook_PreProjectTests/GooglePlayBook_PreProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  GooglePlayBook_PreProjectTests.swift
+//  GooglePlayBook_PreProjectTests
+//
+//  Created by 김소영 on 2023/02/08.
+//  Copyright © 2023 SoYoungKim. All rights reserved.
+//
+
+import XCTest
+@testable import GooglePlayBook_PreProject
+
+class GooglePlayBook_PreProjectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
